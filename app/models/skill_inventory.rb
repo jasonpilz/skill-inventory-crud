c9a87ce0c3c28a879bf4e205fef299f76cc15a65
Jason Pilz <jasonpilz@gmail.com>
2015-10-14 09:16:58 -0600

    Added all files

--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,53 @@
+require 'yaml/store'
+require_relative 'skill'
+
+# CRUD out the functionality for a user to be able to add a skill
+# (id, name, status), see all skills, edit a skill, and delete a skill.
+
 class SkillInventory
   def self.database
     @database ||= YAML::Store.new("db/skill_inventory")
   end
+
+  def self.create(skill)
+    database.transaction do
+      database['skills'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['skills'] << { "id" => database['total'], "name" => skill[:name], "status" => skill[:status] }
+    end
+  end
+
+  def self.all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
+  def self.raw_skills
+    database.transaction do
+      database['skills'] || []
+    end
+  end
+
+  def self.raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def self.find(id)
+    Skill.new(raw_skill(id))
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      target = database['skills'].find { |skill| skill["id"] == id }
+      target["name"] = data[:name]
+      target["status"] = data[:status]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database['skills'].delete_if { |skill| skill["id"] == id }
+    end
+  end
+
 end
